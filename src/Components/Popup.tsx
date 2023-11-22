eb8d6858546c32428c0310e5582941842fa3a688
GodsonKd <godsonshajisimi1972@gmail.com>
2023-11-22 18:28:33 +0530

    commit_6

--- src/Components/Popup.tsx
+++ src/Components/Popup.tsx
@@ -1,4 +1,4 @@
-// PopUp.js
+/* // PopUp.js
 
 
 import './Popup.css';
@@ -34,6 +34,54 @@ const PopUp = ({strCategory,strMeal,strMealThumb,strInstructions,popup,setPopup}
     </div>
 
   
+  );
+};
+
+export default PopUp;
+ */
+
+import React from 'react';
+import './Popup.css';
+
+const PopUp = ({
+  strCategory,
+  strMeal,
+  strMealThumb,
+  strInstructions,
+  popup,
+  setPopup
+}: {
+  strCategory: string;
+  strMeal: string;
+  strMealThumb: string;
+  strInstructions?: string;
+  popup: boolean;
+  setPopup: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  return (
+    <div className={`popupContainer ${popup ? 'active' : ''}`}>
+      <div className="popupContent">
+        <div className="cardContainer">
+          <div className="cardHeader">
+            <div className='descTitle'>
+              <h2 className="title">{strMeal}</h2>
+              <h3 className="category">Category: {strCategory}</h3>
+
+            </div>
+            
+            <img src={strMealThumb} alt={strMeal} className="dishImage" />
+           
+          </div>
+          <div className="cardDescription">
+            <h4>Instructions to prepare</h4>
+            <div className="instruction">{strInstructions}</div>
+          </div>
+        </div>
+        <button className="closeButton" onClick={() => setPopup(false)}>
+          Close
+        </button>
+      </div>
+    </div>
   );
 };
 

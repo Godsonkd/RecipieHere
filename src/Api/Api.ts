a645be73e89afc516d0632affbb02f3dbd4089d1
GodsonKd <godsonshajisimi1972@gmail.com>
2023-11-23 14:44:50 +0530

    commit_7

--- src/Api/Api.ts
+++ src/Api/Api.ts
@@ -9,7 +9,7 @@ export const fetchDishes = async () => {
 
   try {
     
-    const apiUrl="https://www.themealdb.com/api/json/v1/1/search.php?f=c";
+    const apiUrl="https://www.themealdb.com/api/json/v1/1/search.php?f=b";
     const response = await axios.get(apiUrl);
     const dishes=response.data;
     return dishes;
